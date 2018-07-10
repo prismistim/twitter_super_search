64c8cc816c3edbb56797a1a549d7b19e051ff545
Toooooooyaaaaaaaaa <wall.helloworld@gmail.com>
2018-07-10 12:42:21 +0900

    wada

--- getTweet.php
+++ getTweet.php
@@ -9,6 +9,23 @@ if (getenv('ENV_MODE') === 'dev'){
 if (isset($_GET['test-text'])){
 
   $searchWords = $_GET['test-text'];
+  $subOption = $_GET['ninsyo'];
+
+  if($subOption == "認証済み"){
+    $users_params = ['q' => $searchWords ,'count' => '12'];
+    $users = $connection->get('users/search', $users_params)->statuses;
+
+    foreach ($users as $value) {
+      $user = htmlspecialchars($value->text, ENT_QUOTES, 'UTF-8', false);
+      // 検索キーワードをマーキング
+      $keywords = preg_split('/,|\sOR\s/', $tweets_params['q']); //配列化
+      foreach ($keywords as $key) {
+          $text = str_ireplace($key, '<span class="keyword">'.$key.'</span>', $text);
+      }
+      // ツイート表示のHTML生成
+      disp_tweet($value, $text);
+    }
+  }
 
   // キーワードによるツイート検索
   $tweets_params = ['q' => $searchWords ,'count' => '12'];
