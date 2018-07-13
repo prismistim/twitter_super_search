c4076358362bf82c904fe3c4ff43b2459b07da85
prismistim <prismistim@hotmail.com>
2018-07-13 10:02:24 +0900

    コミットを元に戻す

--- index.php
+++ index.php
@@ -93,9 +93,5 @@
   <script src="/js/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-  <script src="/js/oauth.js" type="text/javascript"></script>
-  <script src="/js/sha1.js" type="text/javascript"></script>
-  <script src="/js/localenv.js" type="text/javascript"></script>
-  <script src="/js/getTwits.js" type="text/javascript"></script>
 </body>
 </html>
