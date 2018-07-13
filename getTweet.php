83aadc4c6c90aa9d439d4710658af264031d543c
prismistim <prismistim@hotmail.com>
2018-07-13 10:02:06 +0900

    コミットを元に戻す

--- getTweet.php
+++ getTweet.php
@@ -5,36 +5,14 @@ if (getenv('ENV_MODE') === 'dev'){
 }else{
   require 'oauthServer.php';
 }
-
 if (isset($_GET['test-text'])){
-
   $searchWords = $_GET['test-text'];
-  $subOption = $_GET['ninsyo'];
-
-  if($subOption == "認証済み"){
-    $users_params = ['q' => $searchWords ,'count' => '12'];
-    $users = $connection->get('users/search', $users_params)->statuses;
-
-    foreach ($users as $value) {
-      $user = htmlspecialchars($value->text, ENT_QUOTES, 'UTF-8', false);
-      // 検索キーワードをマーキング
-      $keywords = preg_split('/,|\sOR\s/', $tweets_params['q']); //配列化
-      foreach ($keywords as $key) {
-          $text = str_ireplace($key, '<span class="keyword">'.$key.'</span>', $text);
-      }
-      // ツイート表示のHTML生成
-      disp_tweet($value, $text);
-    }
-  }
-
   // キーワードによるツイート検索
   $tweets_params = ['q' => $searchWords ,'count' => '12'];
   $tweets = $connection->get('search/tweets', $tweets_params)->statuses;
-
   // ニックネームからユーザ情報を取得
   $users_params = ['screen_name' => 'yokoh9'];
   $users = $connection->get('users/show', $users_params);
-
   foreach ($tweets as $value) {
     $text = htmlspecialchars($value->text, ENT_QUOTES, 'UTF-8', false);
     // 検索キーワードをマーキング
@@ -46,7 +24,6 @@ if (isset($_GET['test-text'])){
     disp_tweet($value, $text);
   }
 }
-
 function disp_tweet($value, $text){
   $icon_url = $value->user->profile_image_url;
   $screen_name = $value->user->screen_name;
@@ -54,7 +31,6 @@ function disp_tweet($value, $text){
   $updated = date('Y/m/d H:i', strtotime($value->created_at));
   $tweet_id = $value->id_str;
   $url = 'https://twitter.com/' . $screen_name . '/status/' . $tweet_id;
-
   echo '<div class="col-lg-4">';
   echo '<div class="card">' . PHP_EOL;
   echo '<div class="card-body">';
