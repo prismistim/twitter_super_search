c3fa77459c51e629d369251dab54dd2a24e296dd
prismistim <prismistim@hotmail.com>
2018-07-13 16:51:45 +0900

    ツイート検索を追加

--- getTweet.php
+++ getTweet.php
@@ -7,12 +7,26 @@ if (getenv('ENV_MODE') === 'dev'){
 }
 
 if (isset($_GET['text'])){
-  $searchWords = $_GET['text'];
-  $tweetsParams = ['q' => $searchWords ,'count' => '9'];
-  $optionType = $_GET['options'];
+  $searchWords = $_GET['text']; // キーワードを取得
+  $optionType = $_GET['options']; // 検索したいものを取得
+  // $subOptionType = $_GET['suboptions']; // 検索条件を取得
+
+  $tweetsParams = ['q' => $searchWords ,'count' => '12']; // 検索条件を設定
   
   if($optionType == 'tweets'){
-    
+    // キーワードによるツイート検索
+    $tweets = $connection->get('search/tweets', $tweetsParams)->statuses;
+
+    foreach ($tweets as $value) {
+      $text = htmlspecialchars($value->text, ENT_QUOTES, 'UTF-8', false);
+      // 検索キーワードをマーキング
+      $keywords = preg_split('/,|\sOR\s/', $tweetsParams['q']); //配列化
+      foreach ($keywords as $key) {
+          $text = str_ireplace($key, '<span class="keyword">'.$key.'</span>', $text);
+      }
+      // ツイート表示のHTML生成
+      disp_tweet($value, $text);
+    }
   }else{
     // $connection->get('やりたい事', 検索条件)でユーザー情報を取得
     $users = $connection->get('users/search', $tweetsParams);
@@ -26,14 +40,35 @@ if (isset($_GET['text'])){
           }
           break;
         case 'userId':
-          // TODO:ここに上と同じように書いて、$value->XXXXXのXXXXXを変える。
+          if (strpos($value->screen_name, $searchWords) != false){
+            disp_users($value, $users); //検索結果を表示する関数に渡す
+          }
           break;
         case 'description':
-          // TODO:ここに上と同じように書いて、$value->XXXXXのXXXXXを変える。
+          if (strpos($value->description, $searchWords) != false){
+            disp_users($value, $users); //検索結果を表示する関数に渡す
+          }
           break;
       }
     }
   }
+}
+
+function disp_tweet($value, $text){
+  $icon_url = $value->user->profile_image_url;
+  $icon_url = str_replace('normal', 'bigger', $icon_url); //デフォルトの画像は小さいので大きいものを読み込むため文字を置き換え
+  $screen_name = $value->user->screen_name;
+  $name = $value->user->name;
+  $updated = date('Y/m/d H:i', strtotime($value->created_at));
+  $tweet_id = $value->id_str;
+  $url = 'https://twitter.com/' . $screen_name . '/status/' . $tweet_id;
+  echo '<div class="col-lg-4">';
+  echo '<div class="card">' . PHP_EOL;
+  echo '<div class="card-body">';
+  echo '<div class="thumb">' . '<img alt="" src="' . $icon_url . '">' . '</div>' . PHP_EOL;
+  echo '<div class="meta"><a class="time" target="_blank" href="' . $url . '">' . $updated . '</a>' . '<br><div class="userId">@' . $screen_name .'</div></div>' . PHP_EOL;
+  echo '<div class="tweet">' . $text . '</div>' . PHP_EOL;
+  echo '</div></div></div>' . PHP_EOL;
 }
 
 function disp_users($value, $users){
