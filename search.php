369512e6a1f384e8595539a6c4177f3795b9528a
prismistim <prismistim@hotmail.com>
2018-07-17 19:18:40 +0900

    説明を追加

--- search.php
+++ search.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description" content="Twitterの検索がAPIでちょっと便利になるサービスを目指して">
   <title>Twitter Super Search</title>
 
   <!--CSS 読込み-->
